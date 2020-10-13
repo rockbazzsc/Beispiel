9930a7ae5362c48e8b32e033da5f063e067daa4f
rockbazzsc <72789117+rockbazzsc@users.noreply.github.com>
2020-10-13 10:16:46 +0200

    added second print statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
     System.out.println("sup");
     System.out.println("The predetor");
+    System.out.println(" predetor");
+
     }
 }
